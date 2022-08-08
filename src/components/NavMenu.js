5df8233b1ba8ef140254a01f6bdd4d3829917f9f
Pavel <p-olhovoi@bk.ru>
2022-08-08 10:37:36 +0200

    feat: Show the user name in the menu

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,14 +1,18 @@
 import { Link, NavLink } from "react-router-dom";
+import { connect } from "react-redux/es/exports";
 
 const toggleActive = ({isActive}) => isActive ? "nav-link-active nav-link" : "nav-link"
 
 
-const NavMenu = ()=> {
+const NavMenu = ({authed})=> {
+    const user = authed.status
+    console.log(authed)
     return (
         <header>
             <nav>
                 <div className="menu-col menu-col__left">
                     <span className="logo">The Polls</span>
+
                 </div>
                 <div className="menu-col menu-col__centre">
                 <NavLink className={toggleActive} to="create">Create new poll</NavLink>
@@ -17,12 +21,25 @@ const NavMenu = ()=> {
                 <div className="menu-col menu-col__right">
                     <NavLink to="/" className={toggleActive}>Home</NavLink>
                     <NavLink to="leaderboard" className={toggleActive}>Leaderboard</NavLink>
-                    {/* <span className="nav-link">UserName</span> */}
-                    <NavLink className={toggleActive} to="auth">Login</NavLink>
+
+                    {
+                        user ? <>
+                            <span className="nav-link nav-link-name">{user.trim()}</span>
+                            <span className="nav-link">Logout</span>
+                        </>
+                        : <NavLink className={toggleActive} to="auth">Login</NavLink>
+                    }
                 </div>
             </nav>
         </header>
     )
 }
 
-export default NavMenu;
+const mapStateToProps = (state) => {
+    return {
+      authed: state.authed,
+    }
+  }
+
+
+export default connect(mapStateToProps)(NavMenu);
